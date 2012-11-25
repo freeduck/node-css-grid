3365edc58869db9472e9084781ae6e52aedd20b2
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2012-11-25 22:04:28 +0100

    Starting work on overlay

---
+++ resources/overlay-base.js
@@ -0,0 +1,5 @@
+(function(){
+    var overlay = window.document.createElement('div');
+    overlay.setAttribute('style', 'position:absolute;top: 0; left:0');
+    window.document.appendChild(overlay);
+})();
