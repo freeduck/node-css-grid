79ad888cdda758991184c01215bbc0f684ac82c2
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2012-11-26 22:47:27 +0100

    Display overlay

--- resources/overlay-base.js
+++ resources/overlay-base.js
@@ -1,7 +1,18 @@
 var dev_css_grid = dev_css_grid || {};
 (function(){
+    function getBody(callback){
+	var bodyArray = document.getElementsByTagName('body');
+	if(bodyArray.length == 0){
+	    setTimeout(function(){getBody(callback);}, 100);
+	    return;
+	}
+	callback(bodyArray.item(0));
+    }
     var overlay = window.document.createElement('div');
     overlay.setAttribute('style', 'position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: grey; opacity: 0.5');
-    window.document.appendChild(overlay);
-    dev_css_grid.overlay = overlay;
+
+    getBody(function(body){
+	body.appendChild(overlay);
+	overlay.appendChild(dev_css_grid.gridElement);
+    });
 })();
